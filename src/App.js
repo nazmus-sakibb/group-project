77f12589ee370443b9026f4249031b2f4bc6107a
iamarif1 <iamarif121@gmail.com>
2021-05-29 00:28:22 +0600

    Making Sidebar

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import './'
 
 
 import './App.css';
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
     <div>
-     
+      <Sidebar></Sidebar>
     </div>
   );
 }
