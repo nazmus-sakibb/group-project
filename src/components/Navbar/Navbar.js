b70af9e8572a5b011069104a0f4f8a24e4f66820
dev-nasreen <nasreen.akter34@gmail.com>
2021-05-29 21:21:54 +0600

    card made

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,6 +5,7 @@ import { AiFillFacebook } from 'react-icons/ai';
 import { IoLogoLinkedin } from 'react-icons/io';
 import { IoLogoInstagram } from 'react-icons/io';
 import './Navbar.css'
+import {link} from 'react-router-dom';
 
 
 const Navbar = () => {
@@ -12,36 +13,36 @@ const Navbar = () => {
         <div className="">
             <nav className="navbar navbar-expand-lg navbar-light ">
                 <div className="container">
-                    <a className="navbar-brand" href="#"><img src={logo} style={{ height: "50px" }} alt="" /></a>
+                    <Link className="navbar-brand" <to="#"><img src={logo} style={{ height: "50px" }} alt="" /></Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0 main-menu">
                             <li className="nav-item">
-                                <a className="nav-link" aria-current="page" href="#"><AiOutlineTwitter /></a>
+                                <LinkclassName="nav-link" aria-current="page" <to="#"><AiOutlineTwitter /></Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#"><AiFillFacebook /></a>
+                                <LinkclassName="nav-link" <to="#"><AiFillFacebook /></Link>
                             </li>
 
                             <li className="nav-item">
-                                <a className="nav-link" href="#" tabindex="-1" aria-disabled="true"><IoLogoLinkedin /></a>
+                                <LinkclassName="nav-link" <to="#" tabindex="-1" aria-disabled="true"><IoLogoLinkedin /></Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#" tabindex="-1" aria-disabled="true"><IoLogoInstagram /></a>
+                                <LinkclassName="nav-link" <to="#" tabindex="-1" aria-disabled="true"><IoLogoInstagram /></Link>
                             </li>
                         </ul>
                         <ul className="navbar-nav ms-auto mb-2 mb-lg-0 main-menu">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                                <LinkclassName="nav-link active" aria-current="page" <to="#">Home</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Incubator</a>
+                                <LinkclassName="nav-link" <to="#">Incubator</Link>
                             </li>
 
                             <li className="nav-item">
-                                <a className="nav-link" href="#" tabindex="-1" aria-disabled="true">Learn Page</a>
+                                <LinkclassName="nav-link" <to="#" tabindex="-1" aria-disabled="true">Learn Page</Link>
                             </li>
                         </ul>
 
