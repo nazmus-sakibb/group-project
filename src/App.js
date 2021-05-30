84c95f3c5157ea7d343dd5f480f16561f1f17da8
NAZMUS SAKIB <sakibabir52@gmail.com>
2021-05-30 16:37:34 +0600

    Ready for deployed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 import './App.css';
-// import Sidebar from './Components/Sidebar/Sidebar';
 
 import {
   BrowserRouter as Router,
