1a8cdf832f918568e29abdce5a72e6e2f3cb0a2e
iamarif1 <iamarif121@gmail.com>
2021-06-01 00:33:41 +0600

    Sidebar Updated

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -8,34 +8,36 @@ import "./Sidebar.css";
 const Sidebar = () => {
   return (
     <div className="sidebar d-flex flex-column justify-content-between col-md-3 py-1 px-1 bg-white fw-bold">
-      <p>Founded 17th May, 2015</p>
-      <hr />
-      <small>Website</small>
-      <div className="d-flex">
-        <a href="mangoswap.com">mangoswap.com</a>
-        <div className="ms-3">
-          <a href="#">
-            <AiOutlineTwitter />
-          </a>
-          <a href="#">
-            <FaLinkedinIn />
-          </a>
-          <a href="#">
-            <FaFacebookF />
-          </a>
+      <div className="ms-5">
+        <p>Founded 17th May, 2015</p>
+        <hr />
+        <small>Website</small>
+        <div className="d-flex">
+          <a href="mangoswap.com">mangoswap.com</a>
+          <div className="ms-3">
+            <a href="#">
+              <AiOutlineTwitter />
+            </a>
+            <a href="#">
+              <FaLinkedinIn />
+            </a>
+            <a href="#">
+              <FaFacebookF />
+            </a>
+          </div>
         </div>
+        <small>Location</small>
+        <p>San Francisco, California</p>
+        <small>Team Size</small>
+        <p>100-150 People</p>
+        <hr />
+        <h4>Meet the team</h4>
+        <div>
+          <Team />
+        </div>
+        <hr />
+        <h3>Tags</h3>
       </div>
-      <small>Location</small>
-      <p>San Francisco, California</p>
-      <small>Team Size</small>
-      <p>100-150 People</p>
-      <hr />
-      <h4>Meet the team</h4>
-      <div>
-        <Team />
-      </div>
-      <hr />
-      <h3>Tags</h3>
     </div>
   );
 };
