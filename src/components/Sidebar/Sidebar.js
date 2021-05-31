4ad962767e135a55978c246c33f33755305d213f
iamarif1 <iamarif121@gmail.com>
2021-06-01 00:25:37 +0600

    Sidebar Updated

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,53 +1,43 @@
-import React from 'react';
-import { AiOutlineTwitter } from 'react-icons/ai'
+import React from "react";
+import { AiOutlineTwitter } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaFacebookF } from "react-icons/fa";
-import './Sidebar.css'
-import { Link } from 'react-router-dom';
+import Team from "../Team/Team";
+import "./Sidebar.css";
 
 const Sidebar = () => {
-    return (
-      <div className="sidebar d-flex flex-column justify-content-between col-md-3 py-1 px-1 bg-white">
-        <p>Founded 17th May, 2015</p>
-        <hr />
-        <small>Website</small>
+  return (
+    <div className="sidebar d-flex flex-column justify-content-between col-md-3 py-1 px-1 bg-white fw-bold">
+      <p>Founded 17th May, 2015</p>
+      <hr />
+      <small>Website</small>
+      <div className="d-flex">
         <a href="mangoswap.com">mangoswap.com</a>
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0 social-menu">
-          <li className="nav-item">
-            <Link
-              className="nav-link"
-              aria-current="page"
-              to="https://twitter.com/minorityprogram"
-            >
-              <AiOutlineTwitter />
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              className="nav-link"
-              to="https://www.facebook.com/MinorityProgrammers"
-            >
-              <FaFacebookF />
-            </Link>
-          </li>
-
-          <li className="nav-item">
-            <Link
-              className="nav-link"
-              to="https://www.linkedin.com/company/minority-programmers/"
-            >
-              <FaLinkedinIn />
-            </Link>
-          </li>
-        </ul>
-        <small>Location</small>
-        <p>San Francisco, California</p>
-        <small>Team Size</small>
-        <p>100-150 People</p>
-        <hr />
-        <h4>Meet the team</h4>
+        <div className="ms-3">
+          <a href="#">
+            <AiOutlineTwitter />
+          </a>
+          <a href="#">
+            <FaLinkedinIn />
+          </a>
+          <a href="#">
+            <FaFacebookF />
+          </a>
+        </div>
       </div>
-    );
+      <small>Location</small>
+      <p>San Francisco, California</p>
+      <small>Team Size</small>
+      <p>100-150 People</p>
+      <hr />
+      <h4>Meet the team</h4>
+      <div>
+        <Team />
+      </div>
+      <hr />
+      <h3>Tags</h3>
+    </div>
+  );
 };
 
 export default Sidebar;
