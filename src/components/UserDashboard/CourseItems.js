d6ae453ff3f9a2ce27f8aab879cbf685435c48ef
iamarif1 <iamarif121@gmail.com>
2021-06-01 10:33:09 +0600

    Course Sidebar update

--- src/components/UserDashboard/CourseItems.js
+++ src/components/UserDashboard/CourseItems.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
 import courseModules from '../fakeData/courseModules.json'
+import CourseSidebar from "../CourseSidebar/CourseSidebar";
 
 const CourseItems = () => {
     const { _id } = useParams();
@@ -21,7 +22,7 @@ const CourseItems = () => {
         <div className="">
             <div className="row">
                 <div className="col-md-3 learn-page-container">
-                    <h3>This is sidebar</h3>
+                    <CourseSidebar/>
                 </div>
                 <div className=" col-md-9 px-3">
                     <div className="row">
--- src/components/UserDashboard/UserDashboard.js
+++ src/components/UserDashboard/UserDashboard.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
 import allcourses from '../fakeData/course.json'
+import CourseSidebar from "../CourseSidebar/CourseSidebar";
+
 const UserDashboard = () => {
     const { id } = useParams();
 
@@ -20,7 +22,7 @@ const UserDashboard = () => {
         <div className="">
             <div className="row">
                 <div className="col-md-3 learn-page-container">
-                    <h3>This is sidebar</h3>
+                    <CourseSidebar/>
                 </div>
                 <div className=" col-md-9 px-3">
                     <div className="row">
